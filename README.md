aec242499eb11552be8e3d5a84e43af8526724c5
riaanekaa <148047696+riaanekaa@users.noreply.github.com>
2025-10-15 11:49:05 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 buat controller
-php artisan make:controller
+##membuat controller
+php artisan make:controller -r
 php artisan make:migration 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::get('fakultas', [FakultasController::class, 'index'])->name('fakultas.index');
 
 
 
